94cf11b75d962a02c3429087aaa5bc725edb2fbc
jacobbhartman <209@holbertonschool.com>
2019-01-08 19:26:46 -0800

    Make a pitch to potential coaches, and be able to convert all models from csv to SQLite

--- vbvb/api/populate/parse_csv.py
+++ vbvb/api/populate/parse_csv.py
@@ -1,12 +1,42 @@
 #!/bin/env/python3
 '''
     Run within Python shell after running 'python3 manage.py shell'
+
+    This Python script will convert comma-separated-values (.csv) files, each of which corresponds to a model in the application programming interface (API).
+
+    Its first task is to rename .csv files downloaded from the public Google Sheets
+    spreadsheet into a sanitized .csv file that mimics its corresponding models name.
+
+    Its second task is to open each file and write objects one-by-one to the database. Files have to be opened and used to populate the database in a specific order. That specific order is in order of dependence. Least dependent models are populated first, most dependent models are populated last
+
+    It would be nice if there were a program that automatically wrote models from .csv files but I guess that is another project in itself.
 '''
 
+
+from api.models import Authority, City, Finish, State, Team, Tournament
 from csv import DictReader
-from api.models import City, State
 from django.utils import timezone
+from os import rename
 from uuid import uuid4
+
+# Rename all the .csv files with unsanitary names
+prefix = 'water_polo_public_data - ' # file prefix
+words = ['authorities', 'cities', 'finishes', 'teams', 'tournaments', 'states']
+for word in words:
+    word.append('.csv')
+    rename(prefix+word, word)
+
+
+# Now populate the database with objects based on the .csv file
+with open('populate/authorities.csv') as c:
+    r = DictReader(c)
+    for row in r:
+        p = Authority(created=timezone.now(),
+                      modified=timezone.now(),
+                      uid=uuid4(),
+                      name=row['name'],
+                      kind=row['kind'],)
+        p.save()
 
 with open('populate/states.csv') as c:
     r = DictReader(c)
@@ -19,6 +49,16 @@ with open('populate/states.csv') as c:
                   population=int(row['population_2017'].replace(',', '')),)
         p.save()
 
+with open('populate/tournaments.csv') as c:
+    r = DictReader(c)
+    for row in r:
+        p = Tournament(created=timezone.now(),
+                       modified=timezone.now(),
+                       uid=uuid4(),
+                       name=row['name'],
+                       number_of_teams=int(row['number_of_teams']),)
+        p.save()
+
 with open('populate/cities.csv') as c:
     r = DictReader(c)
     state = State.states.get(name__iexact='California')
@@ -27,5 +67,31 @@ with open('populate/cities.csv') as c:
                  modified=timezone.now(),
                  uid=uuid4(),
                  name=row['name'],
-                 state_id=state,)
+                 id_state=state,)
         p.save()
+
+with open('populate/teams.csv') as c:
+    r = DictReader(c)
+    for row in r:
+        city      = City.cities.get(name__iexact=row['name_city'])
+        authority = Authority.authorities.get(name__iexact=row['name_authority'])
+        p = Team(created=timezone.now(),
+                 modified=timezone.now(),
+                 uid=uuid4(),
+                 name=row['name'],
+                 id_city=city,
+                 id_authority=authority,
+                 kind=row['kind'],)
+        p.save()
+
+with open('populate/finishes.csv') as c:
+    r = DictReader(c)
+    for row in r:
+        tournament = Tournament.tournaments.get(name__iexact=row['name_tournament'])
+        team       = Team.teams.get(name__iexact=row['name_team'])
+        p = Team(created=timezone.now(),
+                 modified=timezone.now(),
+                 uid=uuid4(),
+                 finish=int(row['place']),)
+        p.save()
+
