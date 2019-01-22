649f205133e921bf6ca7dbf1991db13bfcefba45
jacobbhartman <209@holbertonschool.com>
2019-01-22 07:32:01 +0000

    finally fixed bug... must override field->parent with hyperlink

--- vbvb/api/serializers.py
+++ vbvb/api/serializers.py
@@ -12,10 +12,12 @@ from .models import Authority, City, Finish, State, Team, Tournament
 
 
 class AuthoritySerializer(HyperlinkedModelSerializer):
+    
     url = HyperlinkedIdentityField(
         view_name='authority-detail',
         lookup_field='slug'
     )
+    
     teams = HyperlinkedRelatedField(
         view_name='team-detail',
         lookup_field='slug',
@@ -25,8 +27,9 @@ class AuthoritySerializer(HyperlinkedModelSerializer):
     
     class Meta:
         model  = Authority
-        fields = '__all__'
 
+        #fields = ('uid', 'created', 'modified')i
+        fields = ('uid', 'created', 'modified', 'name', 'slug', 'kind', 'url', 'teams')
 
 class CitySerializer(HyperlinkedModelSerializer):
     url = HyperlinkedIdentityField(
@@ -39,25 +42,39 @@ class CitySerializer(HyperlinkedModelSerializer):
         many=True,
         read_only=True
     )
-
+    state = HyperlinkedRelatedField(
+        view_name='state-detail',
+        read_only=True,
+        lookup_field='slug'
+    )
     class Meta:
         model  = City
-        fields = '__all__'
+        fields = ('uid', 'created', 'modified', 'name', 'slug', 'state', 'url', 'teams')
 
 
 class FinishSerializer(HyperlinkedModelSerializer):
     url = HyperlinkedIdentityField(
         view_name='finish-detail'
     )
+    team = HyperlinkedIdentityField(
+        view_name='team-detail',
+        read_only=True,
+        lookup_field='slug',
+    )
+    tournament = HyperlinkedIdentityField(
+        view_name='tournament-detail',
+        read_only=True,
+        lookup_field='slug'
+    )
     class Meta:
         model  = Finish
-        fields = '__all__'
+        fields = ('uid', 'created', 'modified', 'team', 'tournament', 'place', 'url')
 
 
 class StateSerializer(HyperlinkedModelSerializer):
     url = HyperlinkedIdentityField(
         view_name='state-detail',
-        lookup_field='slug'
+        lookup_field='slug',
     )
     cities = HyperlinkedRelatedField(
         view_name='city-detail',
@@ -65,10 +82,11 @@ class StateSerializer(HyperlinkedModelSerializer):
         many=True,
         read_only=True
     )
+  
     
     class Meta:
         model  = State
-        fields = '__all__'
+        fields = ('uid', 'created', 'modified', 'name', 'slug', 'abbreviation', 'population', 'url', 'cities')
 
 
 class TeamSerializer(HyperlinkedModelSerializer):
@@ -82,11 +100,28 @@ class TeamSerializer(HyperlinkedModelSerializer):
         many=True,
         read_only=True
     )
+    authority = HyperlinkedRelatedField(
+        read_only=True,
+        view_name='authority-detail',
+        lookup_field='slug'
+    )
+    city = HyperlinkedRelatedField(
+        read_only=True,
+        view_name='city-detail',
+        lookup_field='slug'
+    )
     
     class Meta:
         model  = Team
-        fields = '__all__'
-
+        fields = ('authority', 'city', 'uid', 'created', 'modified', 'name', 'slug', 'kind', 'url', 'finishes')
+        # 'authority', 'city'
+        '''
+            So I've had a breakthrough discovery. When I put in the two
+            parent models as fields for team, it would attempt to
+            resolve the detail of that authority or city rather than
+            just providing the URL for them. let's see if I can
+            get the urls instead
+        '''
 
 class TournamentSerializer(HyperlinkedModelSerializer):
     url = HyperlinkedIdentityField(
@@ -102,4 +137,4 @@ class TournamentSerializer(HyperlinkedModelSerializer):
     
     class Meta:
         model  = Tournament
-        fields = '__all__'
+        fields = ('uid', 'created', 'modified', 'name', 'slug', 'number_of_teams', 'url', 'finishes')
